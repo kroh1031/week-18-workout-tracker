43556634ebc9cb274d73dd2b3fd004fde2b6ee95
Karen Roh <karenroh@hotmail.com>
2021-06-14 12:14:06 -0400

    set up layout for routes in controllers folder

---
+++ controllers/api/workout-routes.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+
+// All routes starting with /api/workouts
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const routes = require("./controllers");
+
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
@@ -15,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
 });
 
