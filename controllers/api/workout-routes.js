957ba48fbb941711baf7c7ed1d7ff415c65402bd
Karen Roh <karenroh@hotmail.com>
2021-06-15 11:35:24 -0400

    workout added successfully toast pops up via the put request

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -15,7 +15,19 @@ router.post("/", async (req, res) => {
 // update a workout
 router.put("/:id", async (req, res) => {
   try {
-    const newWorkout = await Workout.updateOne({});
+    const newWorkout = await Workout.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          type: req.body.type,
+          name: req.body.name,
+          duration: req.body.duration,
+          weight: req.body.weight,
+          reps: req.body.reps,
+          sets: req.body.sets,
+        },
+      }
+    );
     res.json(newWorkout);
   } catch (err) {
     res.status(500).json(err);
