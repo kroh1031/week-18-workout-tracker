85f9bc6ca0d55e2b2df4df620bea9a9ccaf8cd97
Karen Roh <karenroh@hotmail.com>
2021-06-15 11:17:33 -0400

    able to post a new workout using postman

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -1,5 +1,26 @@
 const router = require("express").Router();
+const { Workout } = require("../../models");
 
 // All routes starting with /api/workouts
+// submit a new workout
+router.post("/", async (req, res) => {
+  try {
+    const newWorkout = await Workout.create(req.body);
+    res.json(newWorkout);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+// update a workout
+router.put("/:id", async (req, res) => {
+  try {
+    const newWorkout = await Workout.updateOne({});
+    res.json(newWorkout);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//
 module.exports = router;
