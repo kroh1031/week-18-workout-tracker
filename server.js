65d77c469088374190c22d1db4ab16b05dce52db
Karen Roh <karenroh@hotmail.com>
2021-06-18 12:28:59 -0400

    added additional properties to mongoose.connect()

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 //Testing Route
